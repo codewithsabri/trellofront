3e9de18a49ea960634d7eb65cf7735e3bd0e27db
codewithsabri <codewithsabri@gmail.com>
2024-06-22 21:31:10 +0200

    update

--- src/app/tasks/task-card/task-card.component.ts
+++ src/app/tasks/task-card/task-card.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommentComponent } from '../../comments/comment/comment.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-task-card',
-  standalone: true,
-  imports: [],
   templateUrl: './task-card.component.html',
-  styleUrl: './task-card.component.scss'
+  styleUrls: ['./task-card.component.scss'],
+  standalone: true, // This component is not standalone
+  imports: [CommentComponent,CommonModule]
 })
 export class TaskCardComponent {
+  showComments = false; // Initially hide the comments
 
+  toggleComments() {
+    this.showComments = !this.showComments; // Toggle the visibility
+  }
 }
